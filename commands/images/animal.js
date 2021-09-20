3a47698d4bf74170f8a0d833d8fb7c3ca5c2fc0a
AnonDev <85408287+anondev-sudo@users.noreply.github.com>
2021-09-20 20:17:45 +0200

    add category to animal.js

--- commands/images/animal.js
+++ commands/images/animal.js
@@ -11,6 +11,7 @@ module.exports = class extends Command {
 		super(...args, {
 			name: "animal",
 			description: "Get random animal",
+			category: "images",
 			args: [
 				{
 					name: "animal",
